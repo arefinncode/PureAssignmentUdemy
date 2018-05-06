468bdfba2ea59ad3eba0a6964843733f45b39402
arefinncode <sultanul.arefin@nordecode.com>
2018-05-06 12:19:37 +0600

    Assignment_01

--- App.js
+++ App.js
@@ -1,58 +1,106 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
+// import {YellowBox} from 'react-native';
+// YellowBox.ignoreWarnings(['Warning: ...']);
+// import {Image } from 'react-native';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    Button
 } from 'react-native';
+import ListItem from './src/components/ListItem/ListItem';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        // this.state = {text: ''};
+        this.state = {
+            placeName: '',
+            places:[]
+        };
+    }
+
+
+    placeNameChangedHandler=val=>{
+        this.setState({
+            placeName:val
+        });
+    };
+
+    placeSubmitHandler=val=> {
+        console.log("Val is :",val);
+        alert("DDDDD");
+        if (this.state.placeName.trim() === "") {
+            return;
+        }
+
+
+        alert("QQQQQQ");
+        this.setState(prevState => {
+            return {
+                places: prevState.places.concat(prevState.placeName)
+            };
+        });
+
+    }
+
+
+    render(){
+        console.log("this.state.places");
+        console.log(this.state);
+        // alert("check previous line");
+        const placesOutput = this.state.places.map((place,i) =>(
+            <ListItem key={i} placeName={place}/>
+        ));
+        return(
+            <View style ={styles.container}>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        placeholder={"An awesome place"}
+                        value={this.state.placeName}
+                        onChangeText={this.placeNameChangedHandler}
+                        style={styles.placeInput}
+                    />
+                    <Button
+                        title="Add"
+                        style={styles.placeButton}
+                        onPress={this.placeSubmitHandler}
+                    />
+
+                </View>
+                <View style={styles.listContainer}>{placesOutput}</View>
+            </View>
+        );
+    }
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container:{
+        flex: 1,
+        padding:26,
+        backgroundColor: '#F5FCFF',
+        alignItems: 'center',
+        justifyContent: 'flex-start'
+    },
+    inputContainer:{
+        width:"100%",
+        flexDirection:"row",
+        justifyContent:"space-between",
+        alignItems:"center"
+    },
+    placeInput:{
+        width:"70%"
+    },
+    placeButton:{
+        width:"30%"
+    },
+    listContainer:{
+        width:"100%"
+    }
 });
